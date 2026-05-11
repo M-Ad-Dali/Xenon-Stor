4233c872783818ff153ce48edbabd419b7eaabfc
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-05-11 16:46:56 +0300

    edit slider

--- resources/views/components/home-slider.blade.php
+++ resources/views/components/home-slider.blade.php
@@ -3,15 +3,17 @@
     <div class="swiper homeSwiper min-h-[85vh]">
 
         <div class="swiper-wrapper">
-
             {{-- هنا يتم حقن السلايدات --}}
             {{ $slot }}
-
         </div>
 
         {{-- Navigation --}}
-        <div class="swiper-button-next text-purple-500"></div>
-        <div class="swiper-button-prev text-purple-500"></div>
+        <div
+            class="swiper-button-next text-purple-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        </div>
+        <div
+            class="swiper-button-prev text-purple-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        </div>
 
         {{-- Pagination --}}
         <div class="swiper-pagination"></div>
@@ -19,4 +21,3 @@
     </div>
 
 </section>
-
