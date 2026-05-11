6033c98cbb783732b4f38962fc43363bde4b5020
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-05-11 16:17:47 +0300

    edit index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
+
+
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('/');
+})->name('logout');
