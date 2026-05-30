edd66d3d2c6e29bec22d6cfad9506873da2ec344
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-05-30 21:29:35 +0300

    fixed

--- resources/views/components/section-header.blade.php
+++ resources/views/components/section-header.blade.php
@@ -1,7 +1,7 @@
 {{-- تفعيل استقبال المتغيرات الأصلية فقط بدون أي قيم افتراضية --}}
 @props(['title', 'description', 'id'])
 
-<div id="{{ $id }}" class="text-center max-w-3xl mx-auto mb-12 px-4 pt-16" >
+<div id="{{ $id }}" class="text-center max-w-3xl mx-auto mb-12 px-4 pt-20" >
     {{-- طباعة العنوان الأصلي الممرر من السكشن --}}
     <h2 class="text-2xl md:text-4xl font-black text-slate-900 dark:text-white tracking-wider mb-3 uppercase">
         {{ $title }}
