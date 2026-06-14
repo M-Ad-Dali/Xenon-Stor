449ac2fd89cf74e08ae6246ee93e4fd78e4bca03
Moka-Ad-Dali <mohammed.addali@gmail.com>
2026-06-14 03:10:45 +0300

    btn create product components

---
+++ resources/views/components/btn-create-products.blade.php
@@ -0,0 +1,6 @@
+<div class="flex gap-2">
+    <a href="{{ route('products.create') }}"
+        class="bg-purple-600 hover:bg-purple-700 text-white px-5 py-2.5 rounded-xl font-bold text-sm transition-all shadow-lg shadow-purple-600/20 cursor-pointer text-center">
+        + {{ __('إضافة منتج') }}
+    </a>
+</div>
