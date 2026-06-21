23802eebd6df0c7e3389a12ccba9ea2aea93826c
M.Ad-Dali <149003522+M-Ad-Dali@users.noreply.github.com>
2026-06-21 20:11:51 +0300

    fixed

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,30 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // نبحث عن العنصر أولاً
+    const homeSwiperElement = document.querySelector(".homeSwiper");
 
-    new Swiper(".homeSwiper", {
-
-        loop: true,
-
-        effect: "fade",
-
-        fadeEffect: {
-            crossFade: true
-        },
-
-        autoplay: {
-            delay: 7000,
-            disableOnInteraction: false
-        },
-
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true
-        },
-
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-        },
-
-    });
-
+    // إذا وجدنا العنصر، نقوم بتشغيل Swiper، وإذا لم نجده (كما في صفحة اللوجن) لا نفعل شيئاً
+    if (homeSwiperElement) {
+        new Swiper(homeSwiperElement, {
+            loop: true,
+            effect: "fade",
+            fadeEffect: {
+                crossFade: true
+            },
+            autoplay: {
+                delay: 7000,
+                disableOnInteraction: false
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            },
+        });
+    }
 });
