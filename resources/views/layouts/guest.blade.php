69e4160639cbb85ba9b6f0b08a788d18dd9caf8d
M.Ad-Dali <149003522+M-Ad-Dali@users.noreply.github.com>
2026-07-04 06:33:30 +0300

    fixed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,10 +23,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&family=Orbitron:wght@700&display=swap"
         rel="stylesheet">
 
-    {{-- Flatpickr --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-
     {{-- Alpine & Scripts --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
